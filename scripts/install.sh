731e1a5970be230f8f1d5ebf0e89ecd5af1434be
Salman Shahid <salmanshahidzia@gmail.com>
2025-09-29 21:15:24 +0000

    pi05 training + aws script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
+
 GIT_LFS_SKIP_SMUDGE=1 uv sync
 GIT_LFS_SKIP_SMUDGE=1 uv pip install -e 
 
 source .venv/bin/activate
 
 # Install behavior for server deploy 
-cd ~/BEHAVIOR-1K/
+cd /workspace/BEHAVIOR-1K/
 uv pip install -e bddl
 uv pip install -e OmniGibson[eval]
--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
     # --overwrite \
     # --exp_name="openpi_$(date +%Y%m%d_%H%M%S)" \
     
@@ -8,11 +10,11 @@
 #     --batch_size=16 \
 #     --num_train_steps=100000
 
-EXP_NAME="openpi_$(date +%Y%m%d_%H%M%S)"
+EXP_NAME="openpi_05_$(date +%Y%m%d_%H%M%S)"
 echo "Experiment name: $EXP_NAME"
 
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train_val.py pi0_b1k \
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train_val.py pi05_b1k \
     --exp_name="$EXP_NAME" \
     --overwrite \
-    --batch_size=16 \
+    --batch_size=64 \
     --num_train_steps=100000
