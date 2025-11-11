e811bb200bd55e0492af6f3b11db02712ba75da1
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-11 19:15:00 +0000

    continue training for 50k more

--- scripts/train_3.sh
+++ scripts/train_3.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-EXP_NAME="openpi_05_$(date +%Y%m%d_%H%M%S)"
+# EXP_NAME="openpi_05_$(date +%Y%m%d_%H%M%S)"
+EXP_NAME="openpi_05_20251110_180314"
 echo "Experiment name: $EXP_NAME"
 
 CUDA_VISIBLE_DEVICES=4,5 XLA_PYTHON_CLIENT_MEM_FRACTION=0.92 OMNIGIBSON_NO_SIGNALS=1 uv run scripts/train_val.py pi05_single_task_w_us \
     --exp_name="$EXP_NAME" \
-    --overwrite \
+    --resume \
     --batch_size=64 \
     --weight_loader.params_path=gs://openpi-assets/checkpoints/pi05_base/params \
-    --num_train_steps=50000 \
-    --val_log_interval=3000
+    --num_train_steps=100000 \
+    --val_log_interval=2000
