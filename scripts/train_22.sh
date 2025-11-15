f25d3653cdb3c752729e22293f14d132b7a32290
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-15 07:16:34 +0000

    return to OG data distribution, but even higher arm loss, lower LR

--- scripts/train_22.sh
+++ scripts/train_22.sh
@@ -12,7 +12,6 @@ aws s3 sync $CKPT_S3_PATH $CKPT_LOCAL_PATH
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 XLA_PYTHON_CLIENT_MEM_FRACTION=0.92 OMNIGIBSON_NO_SIGNALS=1 uv run scripts/train_val.py pi05_b1k_22_TASKS_oversample \
     --exp_name="$EXP_NAME" \
     --resume \
-    --restart_data \
     --batch_size=256 \
     --weight_loader.params_path=gs://openpi-assets/checkpoints/pi05_base/params \
     --force_new_wandb_run \
