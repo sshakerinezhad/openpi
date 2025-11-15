05e9da1df7662e1379c61003881e49a033baf94a
Salman Shahid <salmanshahidzia@gmail.com>
2025-11-15 21:52:49 +0000

    fix pyav dataloader closing bug?

--- scripts/train_22.sh
+++ scripts/train_22.sh
@@ -4,10 +4,10 @@
 EXP_NAME="openpi_05_20251113_045215_resume_again"
 echo "Experiment name: $EXP_NAME"
 
-# Download checkpoint from S3 from original folder into new training recovery folder
-CKPT_S3_PATH="s3://behavior-challenge/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/83000"
-CKPT_LOCAL_PATH="outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215_resume/83000"
-aws s3 sync $CKPT_S3_PATH $CKPT_LOCAL_PATH
+# # Download checkpoint from S3 from original folder into new training recovery folder
+# CKPT_S3_PATH="s3://behavior-challenge/outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215/83000"
+# CKPT_LOCAL_PATH="outputs/checkpoints/pi05_b1k_22_TASKS_oversample/openpi_05_20251113_045215_resume/83000"
+# aws s3 sync $CKPT_S3_PATH $CKPT_LOCAL_PATH
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 XLA_PYTHON_CLIENT_MEM_FRACTION=0.92 OMNIGIBSON_NO_SIGNALS=1 uv run scripts/train_val.py pi05_b1k_22_TASKS_oversample \
     --exp_name="$EXP_NAME" \
